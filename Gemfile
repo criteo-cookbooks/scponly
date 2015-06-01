3ce339589d95233d26e5850b0b0b1547451b6cca
Jeremy MAURO <j.mauro@criteo.com>
2015-06-01 17:43:55 +0200

    First import

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'test-kitchen'
+gem 'kitchen-vagrant'
+gem 'kitchen-vagrant_winrm'
+gem 'goag', :git => 'https://gitlab.criteois.com/ruby-gems/goag.git'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'scponly'
+maintainer       'Criteo'
+maintainer_email 'j_mauro@criteo.com'
+license          'Apache 2.0'
+description      'Installs/Configures scponly'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
