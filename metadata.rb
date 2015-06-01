1f1af9893436a32b386bf6a89333a204101f7bbb
Jeremy MAURO <j.mauro@criteo.com>
2015-06-01 19:25:29 +0200

    Updating kitchen-test

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'scponly'
 maintainer       'Criteo'
-maintainer_email 'j_mauro@criteo.com'
+maintainer_email 'j.mauro@criteo.com'
 license          'Apache 2.0'
 description      'Installs/Configures scponly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'yum-epel'
