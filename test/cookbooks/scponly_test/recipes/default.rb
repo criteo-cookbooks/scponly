d7a2b151124f3ff8d4ae6789467b13adff2cee0f
Jeremy MAURO <j.mauro@criteo.com>
2015-06-02 18:20:16 +0200

    Adding serverspec tests

---
+++ test/cookbooks/scponly_test/recipes/default.rb
@@ -0,0 +1,64 @@
+#
+# Cookbook Name:: scponly_test
+# Recipe:: default
+#
+# Copyright 2015, Criteo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'scponly'
+
+['id_rsa_test2'].each do |name|
+  cookbook_file ::File.join(Chef::Config[:file_cache_path], name) do
+    mode '0600'
+    source name
+  end
+end
+
+file '/tmp/copy_file' do
+  content 'This is a test'
+end
+
+scponly_user 'chroot_test_passwd' do
+  chrooted true
+  # Setting passwd to 'test'
+  password '$6$YQpME/DN$4.h5fNLSg7FLHY3smHzYFCGoI6YpafMyO6QNHMoiGUKePYPSdn9LgSZrxzwLAdtRTgiPhAUZbp0uHcsGGjlJv.'
+end
+
+scponly_user 'chroot_test2_ssh_key' do
+  chrooted true
+  ssh_keys ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDf/WTHmZdrXVbeCDl6Qtt27qcpNZPgTfSgcU6qzJgsPnlBIEddHMZTDziK+MFR2bYfMq1lWUyrZD83nmm/TZRxNAzn8TerEb6ERxsn9TFuTjkq8HmpSbhCq9a+2YlWk/lp/+oeJdZoQmNVB8xQ/g7uvuncxUPkKGHx4Smxeuq6Mw== test2@kitchen-test']
+end
+
+scponly_user 'test_passwd' do
+  chrooted false
+  home '/home/test_passwd/incoming'
+  # Setting passwd to 'test'
+  password '$6$YQpME/DN$4.h5fNLSg7FLHY3smHzYFCGoI6YpafMyO6QNHMoiGUKePYPSdn9LgSZrxzwLAdtRTgiPhAUZbp0uHcsGGjlJv.'
+end
+
+scponly_user 'test2_ssh_key' do
+  chrooted false
+  home '/home/test2_ssh_key/incoming'
+  ssh_keys ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDf/WTHmZdrXVbeCDl6Qtt27qcpNZPgTfSgcU6qzJgsPnlBIEddHMZTDziK+MFR2bYfMq1lWUyrZD83nmm/TZRxNAzn8TerEb6ERxsn9TFuTjkq8HmpSbhCq9a+2YlWk/lp/+oeJdZoQmNVB8xQ/g7uvuncxUPkKGHx4Smxeuq6Mw== test2@kitchen-test']
+end
+
+scp_args = "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i #{::File.join(Chef::Config[:file_cache_path], 'id_rsa_test2')}"
+execute 'Testing user "chroot_test2_ssh_key"' do
+  command "scp #{scp_args} /tmp/copy_file chroot_test2_ssh_key@localhost:"
+end
+
+execute 'Testing user "test2_ssh_key"' do
+  command "scp #{scp_args} /tmp/copy_file test2_ssh_key@localhost:"
+end
---
+++ test/integration/default/serverspec/check_user_spec.rb
@@ -0,0 +1,71 @@
+#
+# Cookbook Name:: scponly
+# Description::		ServerSpec recipe
+# Recipe::				check_user_spec
+# Author::        Jeremy MAURO (j.mauro@criteo.com)
+#
+#
+#
+
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe package('scponly') do
+  it { should be_installed }
+end
+
+describe 'Checking no chroot user' do
+  describe 'user with password' do
+    describe user('test_passwd') do
+      it { should exist }
+      it { should have_login_shell '/usr/bin/scponly' }
+      it { should have_home_directory '/home/test_passwd/incoming' }
+    end
+    describe file('/home/test_passwd/incoming') do
+      it { should be_directory }
+      it { should be_owned_by 'test_passwd' }
+    end
+  end
+
+  describe 'user with no password' do
+    describe user('test2_ssh_key') do
+      it { should exist }
+      it { should have_login_shell '/usr/bin/scponly' }
+      it { should have_home_directory '/home/test2_ssh_key/incoming' }
+      it { should have_authorized_key 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDf/WTHmZdrXVbeCDl6Qtt27qcpNZPgTfSgcU6qzJgsPnlBIEddHMZTDziK+MFR2bYfMq1lWUyrZD83nmm/TZRxNAzn8TerEb6ERxsn9TFuTjkq8HmpSbhCq9a+2YlWk/lp/+oeJdZoQmNVB8xQ/g7uvuncxUPkKGHx4Smxeuq6Mw== test2@kitchen-test' }
+    end
+    describe file('/home/test2_ssh_key/incoming/copy_file') do
+      it { should be_file }
+      it { should contain 'This is a test' }
+    end
+  end
+end
+
+describe 'Checking chroot user' do
+  describe 'user with password' do
+    describe user('chroot_test_passwd') do
+      it { should exist }
+      it { should have_login_shell '/usr/sbin/scponlyc' }
+      it { should have_home_directory '/var/opt/scponly-chroot/chroot_test_passwd//incoming' }
+    end
+    describe file('/var/opt/scponly-chroot/chroot_test_passwd/incoming') do
+      it { should be_directory }
+      it { should be_owned_by 'chroot_test_passwd' }
+    end
+  end
+
+  describe 'user with no password' do
+    describe user('chroot_test2_ssh_key') do
+      it { should exist }
+      it { should have_login_shell '/usr/sbin/scponlyc' }
+      it { should have_home_directory '/var/opt/scponly-chroot/chroot_test2_ssh_key//incoming' }
+      it { should have_authorized_key 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDf/WTHmZdrXVbeCDl6Qtt27qcpNZPgTfSgcU6qzJgsPnlBIEddHMZTDziK+MFR2bYfMq1lWUyrZD83nmm/TZRxNAzn8TerEb6ERxsn9TFuTjkq8HmpSbhCq9a+2YlWk/lp/+oeJdZoQmNVB8xQ/g7uvuncxUPkKGHx4Smxeuq6Mw== test2@kitchen-test' }
+    end
+    describe file('/var/opt/scponly-chroot/chroot_test2_ssh_key/incoming//copy_file') do
+      it { should be_file }
+      it { should contain 'This is a test' }
+    end
+  end
+end
