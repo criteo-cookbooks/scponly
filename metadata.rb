b35affa9865aad71aa653b7d9d68d7a78d14897b
Jeremy MAURO <j.mauro@criteo.com>
2016-05-13 14:30:52 +0200

    Testing if :issue_url/:source_url tags exist in metadata

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer       'Criteo'
 maintainer_email 'j.mauro@criteo.com'
 license          'Apache 2.0'
 description      'Installs/Configures scponly'
-issues_url       'https://github.com/criteo-cookbooks/scponly/issues'
-source_url       'https://github.com/criteo-cookbooks/scponly'
+issues_url       'https://github.com/criteo-cookbooks/scponly/issues' if respond_to?(:issue_url)
+source_url       'https://github.com/criteo-cookbooks/scponly' if respond_to?(:source_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 depends          'yum-epel'
