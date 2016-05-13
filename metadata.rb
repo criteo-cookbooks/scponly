0b84584d4f3a0be5979521e4f059c37541d3a8a3
Jeremy MAURO <j.mauro@criteo.com>
2016-05-13 13:16:09 +0200

    [Breaking] Bumping version
    * Since the attribute for setting package version

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures scponly'
 issues_url       'https://github.com/criteo-cookbooks/scponly/issues'
 source_url       'https://github.com/criteo-cookbooks/scponly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '1.0.0'
 depends          'yum-epel'
