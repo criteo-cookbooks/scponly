3bfb26e58f2cdd0a144241591de1d12cfe57f11e
Jeremy MAURO <j.mauro@criteo.com>
2016-05-13 13:23:26 +0200

    Adding the supported OS

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,6 @@ source_url       'https://github.com/criteo-cookbooks/scponly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 depends          'yum-epel'
+%w(centos rhel).each do |os|
+  supports os
+end
